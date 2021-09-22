d8a382fed9234e7ec7cb965af0f4e3ed64f9f9bc
Austin Terry <pdkkid1@gmail.com>
2021-09-22 13:31:26 -0600

    added a responsive button to scroll back to top of page

--- src/components/book-list.tsx
+++ src/components/book-list.tsx
@@ -1,9 +1,9 @@
 import { IBook } from "../api/base.types";
-import { Card, Space } from "antd";
+import { BackTop, Card, Space } from "antd";
 import Truncate from "react-truncate";
 import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 import { Favorited } from "./book-favorite";
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import { AppContext } from "../app-context";
 const { Meta } = Card;
 
@@ -17,54 +17,57 @@ const BookList = ({ books }: { books: IBook[] | undefined }) => {
   };
 
   return (
-    <Space
-      size={[12, 16]}
-      wrap
-      align="start"
-      style={{ justifyContent: "space-evenly" }}
-    >
-      {books?.map(book => {
-        return (
-          <Card
-            key={book.id}
-            extra={
-              app.loggedIn &&
-              (Favorited(book) ? (
-                <HeartFilled onClick={() => handleFavoriteToggle(book)} />
-              ) : (
-                <HeartOutlined onClick={() => handleFavoriteToggle(book)} />
-              ))
-            }
-            title={<Truncate lines={2}>{book.volumeInfo.title}</Truncate>}
-            headStyle={{ textAlign: "center" }}
-            style={{ width: 225, flex: 1 }}
-            type="inner"
-            hoverable
-            cover={
-              <img
-                height={300}
-                alt={book.volumeInfo.title}
-                src={
-                  book.volumeInfo.imageLinks?.smallThumbnail ||
-                  "https://i.ibb.co/LxkmGDX/download.png"
+    <Fragment>
+      <BackTop />
+      <Space
+        size={[12, 16]}
+        wrap
+        align="start"
+        style={{ justifyContent: "space-evenly" }}
+      >
+        {books?.map(book => {
+          return (
+            <Card
+              key={book.id}
+              extra={
+                app.loggedIn &&
+                (Favorited(book) ? (
+                  <HeartFilled onClick={() => handleFavoriteToggle(book)} />
+                ) : (
+                  <HeartOutlined onClick={() => handleFavoriteToggle(book)} />
+                ))
+              }
+              title={<Truncate lines={2}>{book.volumeInfo.title}</Truncate>}
+              headStyle={{ textAlign: "center" }}
+              style={{ width: 225, flex: 1 }}
+              type="inner"
+              hoverable
+              cover={
+                <img
+                  height={300}
+                  alt={book.volumeInfo.title}
+                  src={
+                    book.volumeInfo.imageLinks?.smallThumbnail ||
+                    "https://i.ibb.co/LxkmGDX/download.png"
+                  }
+                />
+              }
+            >
+              <Meta
+                title={
+                  <span style={{ fontSize: "12px" }}>
+                    {book.volumeInfo.authors}
+                  </span>
+                }
+                description={
+                  <Truncate lines={4}>{book.volumeInfo.description}</Truncate>
                 }
               />
-            }
-          >
-            <Meta
-              title={
-                <span style={{ fontSize: "12px" }}>
-                  {book.volumeInfo.authors}
-                </span>
-              }
-              description={
-                <Truncate lines={4}>{book.volumeInfo.description}</Truncate>
-              }
-            />
-          </Card>
-        );
-      })}
-    </Space>
+            </Card>
+          );
+        })}
+      </Space>
+    </Fragment>
   );
 };
 
