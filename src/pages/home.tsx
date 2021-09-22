9605a0d981237ed9304f6c18af7bcf662115efc8
Austin Terry <pdkkid1@gmail.com>
2021-09-22 13:31:33 -0600

    added basic home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,21 @@
+import { BookOutlined } from "@ant-design/icons";
+import { Typography, Space } from "antd";
+
+const { Text } = Typography;
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <Space
+      align="center"
+      direction="vertical"
+      size={12}
+      style={{ width: "100%", padding: "16px" }}
+    >
+      <BookOutlined style={{ textAlign: "center", fontSize: "8rem" }} />
+      <Text strong>Books Demo</Text>
+      <Text>Navigate to the Search tab to get started!</Text>
+    </Space>
+  );
 };
 
 export default HomePage;
