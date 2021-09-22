a2554e8fb4c2491feffaefc88f15889332c3445f
Austin Terry <pdkkid1@gmail.com>
2021-09-21 21:18:38 -0600

    created profile page to show name & fav books

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,47 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Divider, Empty } from "antd";
+import { Fragment, useContext } from "react";
+import { AppContext } from "../app-context";
+import { Typography } from "antd";
+import BookList from "../components/book-list";
+
+const { Title } = Typography;
+
 const ProfilePage = () => {
-  return <div>Profile</div>;
+  const { app } = useContext(AppContext);
+  const { favBooks, name, loggedIn } = app;
+
+  return (
+    <Fragment>
+      {loggedIn ? (
+        <Fragment>
+          <Avatar size={64} icon={<UserOutlined />} />
+          <Title
+            level={2}
+            style={{
+              display: "inline",
+              margin: "0 16px",
+              verticalAlign: "middle",
+            }}
+          >
+            Hello, {name}
+          </Title>
+          <Divider>Your Favorites</Divider>
+          {favBooks.length > 0 ? (
+            <BookList books={favBooks} />
+          ) : (
+            <Empty
+              image={Empty.PRESENTED_IMAGE_SIMPLE}
+              description="Nothing here yet"
+            />
+          )}
+        </Fragment>
+      ) : (
+        <div>You are not logged in.</div>
+      )}
+    </Fragment>
+  );
 };
 
+//<BookList books={data?.items} />
 export default ProfilePage;
